d3c16e7589099097d5e59c452d5d2574a685d4cd
Kai Uwe Broulik <kde@privat.broulik.de>
2026-07-01 12:04:12 +0200

    Split prompt into its own static library
    So we can reuse it for unit testing.

---
+++ src/prompt.h
@@ -0,0 +1,27 @@
+/*
+ *   SPDX-FileCopyrightText: 2006 Hans van Leeuwen <hanz@hanz.nl>
+ *   SPDX-FileCopyrightText: 2008-2010 Armin Berres <armin@space-based.de>
+ *   SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#pragma once
+
+class QString;
+
+// Standard prompt types defined by openssh.
+enum class PromptType {
+    Confirm,
+    Entry,
+    None,
+};
+
+// Implemented UI display types.
+enum class DisplayType {
+    Password,
+    ClearText,
+    Confirm,
+    ConfirmCancel,
+    UnknownSshHost
+};
+
+void parsePrompt(PromptType promptType, const QString &prompt, QString &identifier, bool &ignoreKeychain, DisplayType &displayType);
