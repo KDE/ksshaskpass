b99b1a027b6b3a5e0704d17c6357e20de8e918d8
Kai Uwe Broulik <kde@privat.broulik.de>
2026-07-01 14:37:25 +0200

    Handle unknown host when key is known for someone else
    In addition to "This key is not known by any other names" also handle
    "This host key is known by the following other names/addresses" by
    making the rationale part of the regular expression generic.

--- src/prompt.cpp
+++ src/prompt.cpp
@@ -209,7 +209,7 @@ void parsePrompt(PromptType promptType, const QString &prompt, QString &identifi
     // Case: unknown SSH host
     match = QRegularExpression(QStringLiteral("^The authenticity of host '([^']+)(?: \\(([^)]+)\\))?' can't be established\\.\n"
                                               "([A-Z0-9_-]+) key fingerprint is:? ([A-Za-z0-9:+/=]+)\\.\n"
-                                              "This key is not known by any other names\\.\n"
+                                              "(?:.*\n)*"
                                               "Are you sure you want to continue connecting \\(yes/no/\\[fingerprint\\]\\)\\?\\s*$"))
                 .match(prompt);
     if (match.hasMatch()) {
